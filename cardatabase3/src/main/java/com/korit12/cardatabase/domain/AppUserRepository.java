0fb591e390e13ab53b6ea32e6dc3836795345560
maybeags <suren0709@gmail.com>
2026-02-24 12:44:05 +0900

    feat: authentication

---
+++ cardatabase3/src/main/java/com/korit12/cardatabase/domain/AppUserRepository.java
@@ -0,0 +1,6 @@
+package com.korit12.cardatabase.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+}
