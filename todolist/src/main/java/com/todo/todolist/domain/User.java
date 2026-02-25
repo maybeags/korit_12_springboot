2960ab3a469fffd83b20823656a4a992797ba131
maybeags <suren0709@gmail.com>
2026-02-25 17:14:24 +0900

    feat: todo review

---
+++ todolist/src/main/java/com/todo/todolist/domain/User.java
@@ -0,0 +1,36 @@
+package com.todo.todolist.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name= "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Todo> todos;
+
+    public User() {}
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
