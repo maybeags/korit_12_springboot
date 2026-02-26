9f6b71612eddaf50132cdcc0c629381658c4c6f7
maybeags <suren0709@gmail.com>
2026-02-26 17:48:11 +0900

    feat: 20260226 3-Tier Architecture / DTO

--- todolist2/src/main/java/com/todo/todolist/controller/TodoController.java
+++ todolist2/src/main/java/com/todo/todolist/controller/TodoController.java
@@ -24,6 +24,7 @@ public class TodoController {
     }
 
     // todo 조회 API
+    @GetMapping
     public ResponseEntity<List<TodoResponse>> getAllTodos() {
         return ResponseEntity.ok(todoService.getTodoList());
     }
